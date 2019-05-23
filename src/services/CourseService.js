f5ab88281449e4802c1620c87e4e8c3349ec453e
aperez809 <32823645+aperez809@users.noreply.github.com>
2019-05-23 09:08:20 -0400

    added createCourse()

--- src/services/CourseService.js
+++ src/services/CourseService.js
@@ -1,2 +1,27 @@
 import courses from '../components/courses.json';
 
+const courseJson = courses;
+
+
+function createCourse(course) {
+    courseJson.push(course);
+    return courseJson;
+}
+
+
+function findAllCourses() {
+    return null;
+}
+
+function findCourseById(id) {
+    return null;
+}
+
+function updateCourse(course) {
+    return null;
+}
+
+function deleteCourse(course) {
+    return null;
+}
+
